5858e04725d821d658596f8f6000d296c32d7b35
Ugo Casalone <ugo.casalone@gmail.com>
2018-08-06 12:46:49 +0200

    If raise_if_conflicts error, launch gem list middleman. Remove everything. Reinstall middleman and then middleman init my_project --template=ugocasalone/middleman-mu

--- config.rb
+++ config.rb
@@ -10,7 +10,7 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 activate :livereload
-# activate :sprockets
+activate :sprockets
 #-------------------------------------------------------------------------------
 # Layouts
 # https://middlemanapp.com/basics/layouts/
